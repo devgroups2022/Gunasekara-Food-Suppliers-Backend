4a03b1d64e8f358c4449fa8fb4db42486644020d
devgroups2022 <112858799+devgroups2022@users.noreply.github.com>
2022-09-06 10:56:23 +0530

    Add files via upload

---
+++ connection.js
@@ -0,0 +1,11 @@
+// db connections
+const {Client} = require('pg')
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+})
+
+module.exports = client
