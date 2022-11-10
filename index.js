a6ea30dda60ed63eb5d12df86d4c3bd9fa7fdab1
devgroups2022 <112858799+devgroups2022@users.noreply.github.com>
2022-11-10 11:55:11 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const client = require("./connection.js");
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 8080;
+app.use(cors());
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
